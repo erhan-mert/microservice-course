d751190b5389d31b1fb06817b136baf43232663c
Binnur KURT <deepcloudlabs@gmail.com>
2022-06-21 14:28:09 +0300

    add jpa entity and repository adapter

--- hr-microservice/src/main/java/com/example/hr/service/HrService.java
+++ hr-microservice/src/main/java/com/example/hr/service/HrService.java
@@ -1,9 +1,13 @@
 package com.example.hr.service;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.example.hr.application.HrApplication;
+import com.example.hr.application.business.exception.EmployeeNotFoundException;
+import com.example.hr.domain.Employee;
+import com.example.hr.domain.TcKimlikNo;
 import com.example.hr.dto.request.HireEmployeeRequest;
 import com.example.hr.dto.response.EmployeeResponse;
 import com.example.hr.dto.response.FireEmployeeResponse;
@@ -11,27 +15,35 @@ import com.example.hr.dto.response.HireEmployeeResponse;
 
 @Service
 public class HrService {
-    private HrApplication hrApplication;
-    
-	public HrService(HrApplication hrApplication) {
+	private HrApplication hrApplication;
+	private ModelMapper modelMapper;
+
+	public HrService(HrApplication hrApplication, ModelMapper modelMapper) {
 		this.hrApplication = hrApplication;
+		this.modelMapper = modelMapper;
 	}
 
 	@Transactional
 	public HireEmployeeResponse hireEmployee(HireEmployeeRequest request) {
-		
-		return null;
+		var employee = modelMapper.map(request, Employee.class);
+		hrApplication.hireEmployee(employee);
+		return new HireEmployeeResponse("success");
 	}
 
 	@Transactional
 	public FireEmployeeResponse fireEmployee(String identity) {
-		// TODO Auto-generated method stub
-		return null;
+		TcKimlikNo kimlik = TcKimlikNo.valueOf(identity);
+		var firedEmployee = hrApplication.fireEmployee(kimlik);
+		if (firedEmployee.isEmpty())
+			throw new EmployeeNotFoundException(kimlik);
+		return new FireEmployeeResponse("success");
 	}
 
 	public EmployeeResponse findEmployee(String identity) {
-		// TODO Auto-generated method stub
-		return null;
+		TcKimlikNo kimlik = TcKimlikNo.valueOf(identity);
+		return hrApplication.getEmployeeInformation(kimlik)
+				            .map( emp -> modelMapper.map(emp, EmployeeResponse.class))
+				            .orElseThrow( () -> new EmployeeNotFoundException(kimlik));
 	}
 
 }
